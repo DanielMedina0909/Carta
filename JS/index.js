1dbff0cc91ae4222ff894fdcd08a94015bed65dc
DanielMedina0909 <155011383+DanielMedina0909@users.noreply.github.com>
2023-12-27 21:44:42 -0400

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+function miFuncion() {
+   window.location.href = '2index.html';
+}
+function miFuncion2(){
+   window.location.href = 'index.html';
+}
+document.getElementById("BVer").addEventListener("click", function() {
+   document.getElementById("BVer").style.display = "none";
+ });
